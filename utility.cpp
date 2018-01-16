aa3bfed4046b064a183b135d6fdcd89a866f6fdd
Dario Sitnik <dario.sitnik@gmail.com>
2018-01-16 09:48:36 +0100

    0. row and 0. column fix

--- utility.cpp
+++ utility.cpp
@@ -471,17 +471,17 @@ sequences padding function to match block dimensions
 */
 void padding(std::string& a, std::string& b, int M, int N)
 {
-    if (a.length() % M != 0)
+    if (a.length()+1 % M != 0)
         {
-            if (b.length() % N != 0)
+            if (b.length()+1 % N != 0)
             {   
                 char pa = findPadChar(a.back(),b.back(), 'N');
                 char pb = findPadChar(a.back(),b.back(), pa);
-                while (a.length() % M != 0)
+                while (a.length()+1 % M != 0)
                 {
                     a += pa;
                 }
-                while (b.length() % N != 0)
+                while (b.length()+1 % N != 0)
                 {
                     b += pb;
                 }           
@@ -490,7 +490,7 @@ void padding(std::string& a, std::string& b, int M, int N)
             {
         
                 char pa = findPadChar(a.back(),b.back(), 'N');
-                while (a.length() % M != 0)
+                while (a.length()+1 % M != 0)
                 {
                     a += pa;
                 }           
@@ -498,12 +498,12 @@ void padding(std::string& a, std::string& b, int M, int N)
         }
     else
     {
-        if (b.length() % N != 0)
+        if (b.length()+1 % N != 0)
         {
         std::cout<<a.back()<<" "<<b.back()<<std::endl;
             char pb = findPadChar(a.back(),b.back(), 'N');
         std::cout<<pb<<std::endl;
-            while (b.length() % N != 0)
+            while (b.length()+1 % N != 0)
                 {
                     b += pb;
                 }
